0d468d44e4e66719c45abf77d881dee6878aa8e4
guvenaltunsoyy <guven.altunsoy@outlook.com>
2019-11-27 00:04:48 +0300

    initial commit

---
+++ yazlab-multithreading-project/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace yazlab_multithreading_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStopRequest = new System.Windows.Forms.Button();
+            this.myFlow = new System.Windows.Forms.FlowLayoutPanel();
+            this.myConsole = new System.Windows.Forms.RichTextBox();
+            this.mainServerProgress = new System.Windows.Forms.ProgressBar();
+            this.myConsole2 = new System.Windows.Forms.RichTextBox();
+            this.lblMainServer = new System.Windows.Forms.Label();
+            this.lblSubServers = new System.Windows.Forms.Label();
+            this.myFlow.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(928, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStopRequest
+            // 
+            this.btnStopRequest.Location = new System.Drawing.Point(928, 48);
+            this.btnStopRequest.Name = "btnStopRequest";
+            this.btnStopRequest.Size = new System.Drawing.Size(75, 23);
+            this.btnStopRequest.TabIndex = 0;
+            this.btnStopRequest.Text = "Stop";
+            this.btnStopRequest.UseVisualStyleBackColor = true;
+            this.btnStopRequest.Click += new System.EventHandler(this.btnStopRequest_Click);
+            // 
+            // myFlow
+            // 
+            this.myFlow.Controls.Add(this.myConsole);
+            this.myFlow.Controls.Add(this.lblSubServers);
+            this.myFlow.Controls.Add(this.lblMainServer);
+            this.myFlow.Controls.Add(this.mainServerProgress);
+            this.myFlow.Controls.Add(this.myConsole2);
+            this.myFlow.Location = new System.Drawing.Point(13, 13);
+            this.myFlow.Name = "myFlow";
+            this.myFlow.Size = new System.Drawing.Size(909, 565);
+            this.myFlow.TabIndex = 1;
+            // 
+            // myConsole
+            // 
+            this.myConsole.BackColor = System.Drawing.SystemColors.MenuText;
+            this.myConsole.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.myConsole.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.myConsole.Location = new System.Drawing.Point(3, 3);
+            this.myConsole.Name = "myConsole";
+            this.myConsole.Size = new System.Drawing.Size(424, 264);
+            this.myConsole.TabIndex = 1;
+            this.myConsole.Text = "";
+            // 
+            // mainServerProgress
+            // 
+            this.mainServerProgress.Location = new System.Drawing.Point(515, 3);
+            this.mainServerProgress.Name = "mainServerProgress";
+            this.mainServerProgress.Size = new System.Drawing.Size(100, 23);
+            this.mainServerProgress.TabIndex = 0;
+            // 
+            // myConsole2
+            // 
+            this.myConsole2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.myConsole2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.myConsole2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.myConsole2.Location = new System.Drawing.Point(3, 273);
+            this.myConsole2.Name = "myConsole2";
+            this.myConsole2.Size = new System.Drawing.Size(424, 264);
+            this.myConsole2.TabIndex = 1;
+            this.myConsole2.Text = "";
+            // 
+            // lblMainServer
+            // 
+            this.lblMainServer.AutoSize = true;
+            this.lblMainServer.Location = new System.Drawing.Point(474, 0);
+            this.lblMainServer.Name = "lblMainServer";
+            this.lblMainServer.Size = new System.Drawing.Size(35, 13);
+            this.lblMainServer.TabIndex = 2;
+            this.lblMainServer.Text = "label1";
+            // 
+            // lblSubServers
+            // 
+            this.lblSubServers.AutoSize = true;
+            this.lblSubServers.Location = new System.Drawing.Point(433, 0);
+            this.lblSubServers.Name = "lblSubServers";
+            this.lblSubServers.Size = new System.Drawing.Size(35, 13);
+            this.lblSubServers.TabIndex = 3;
+            this.lblSubServers.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 628);
+            this.Controls.Add(this.myFlow);
+            this.Controls.Add(this.btnStopRequest);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.myFlow.ResumeLayout(false);
+            this.myFlow.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStopRequest;
+        private System.Windows.Forms.FlowLayoutPanel myFlow;
+        private System.Windows.Forms.ProgressBar mainServerProgress;
+        private System.Windows.Forms.RichTextBox myConsole;
+        private System.Windows.Forms.RichTextBox myConsole2;
+        private System.Windows.Forms.Label lblSubServers;
+        private System.Windows.Forms.Label lblMainServer;
+    }
+}
+
---
+++ yazlab-multithreading-project/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yazlab_multithreading_project
+{
+    public struct Server
+    {
+        public int RequestCount { get; set; }
+        public int Capacity { get; set; }
+        public bool IsAvaible { get; set; }
+        public CancellationTokenSource CancellationToken { get; set; }
+    };
+    public partial class Form1 : Form, INotifyPropertyChanged
+    {
+        public List<Server> SubServers;
+        public Server MainServer;
+        private CancellationTokenSource cancellationToken;
+        private string _foo;
+
+        public string Foo
+        {
+            get { return _foo; }
+            set
+            {
+                _foo = value;
+                OnPropertyChanged("Foo");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblSubServers.DataBindings.Add(new Binding("Text", this, "Foo"));
+            SubServers = new List<Server>();
+            SubServers.Add(new Server()
+            {
+                RequestCount = 10,
+                IsAvaible = true,
+                Capacity = 5000,
+                CancellationToken = new CancellationTokenSource()
+            });
+            SubServers.Add(new Server()
+            {
+                Capacity = 5000,
+                IsAvaible = true,
+                RequestCount = 10,
+                CancellationToken = new CancellationTokenSource()
+            });
+
+            cancellationToken = new CancellationTokenSource();
+
+            MainServer = new Server()
+            {
+                IsAvaible = true,
+                Capacity = 10000,
+                RequestCount = 0,
+                CancellationToken = new CancellationTokenSource()
+            };
+            RichTextBox richTextBox = myConsole;
+            RichTextBox richTextBox2 = myConsole2;
+            richTextBox.BackColor = Color.Black;
+            richTextBox.ForeColor = Color.Lime;
+            richTextBox2.BackColor = Color.Black;
+            richTextBox2.ForeColor = Color.Lime;
+            richTextBox.Text = "MyConsole >.. \n";
+            richTextBox2.Text = "MyConsole2 >.. \n";
+        }
+        public string LabelSubServer
+        {
+            get
+            {
+                return this.lblSubServers.Text;
+            }
+            set
+            {
+                this.lblSubServers.Text = value;
+            }
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            cancellationToken = new CancellationTokenSource();
+            MainServer.CancellationToken = new CancellationTokenSource();
+            AddRequestToMainServer();
+            CheckSubServerCapacity();
+        }
+        public async Task AddRequestToMainServer()
+        {
+            Random randomNumber = new Random();
+            Task addRequest = Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    MainServer.CancellationToken.Token.ThrowIfCancellationRequested();
+                    MainServer.RequestCount += randomNumber.Next(1, 100);
+                    await MainServerController();
+                    //Console.WriteLine("Added Request - >Main Server Request Count :{0}", MainServer.RequestCount);
+                    await Task.Delay(100);
+                }
+            }
+            , MainServer.CancellationToken.Token);
+
+            Task responseRequest = Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    MainServer.CancellationToken.Token.ThrowIfCancellationRequested();
+                    MainServer.RequestCount -= randomNumber.Next(1, 50);
+                    //Console.WriteLine("Response Request - >Main Server Request Count :{0}", MainServer.RequestCount);
+                    await Task.Delay(200);
+                }
+            }
+            , MainServer.CancellationToken.Token);
+
+            MainServer.CancellationToken.Token.Register(() =>
+            {
+                Console.WriteLine("request stop");
+            });
+        }
+
+        public async Task MainServerController()
+        {
+            //Console.WriteLine("Main Server Request Count :" + MainServer.RequestCount.ToString());
+            if (MainServer.RequestCount >= (MainServer.Capacity * 0.20))
+            {
+                //Console.WriteLine("MAIN SERVER 70 PERCENT FULL");
+                await CallSubServer();
+            }
+        }
+        public async Task CallSubServer()
+        {
+            bool isThereAvaibleServer = false;
+            MainServer.RequestCount -= 50;
+            SubServers.ForEach(server =>
+            {
+                if (server.IsAvaible && !server.CancellationToken.IsCancellationRequested && !isThereAvaibleServer)
+                {
+                    int count = server.RequestCount + 50;
+                    server = new Server() {
+                        Capacity = 5000,
+                        RequestCount = count,
+                        CancellationToken = new CancellationTokenSource(),
+                        IsAvaible = count > 5000 ? false : true
+                    };
+                    server.RequestCount = server.RequestCount + 50;
+                    isThereAvaibleServer = true;
+                    return;
+                }
+            });
+            if (!isThereAvaibleServer)
+            {
+                Console.WriteLine("called createSubServer");
+                await CreateSubServer(50);
+            }
+        }
+        public async Task CheckSubServerCapacity()
+        {
+            Console.WriteLine("checkSubServer CAPACITY");
+            bool isThereAvaibleSubServer = false;
+            Label txt = null;
+            txt = lblSubServers;
+            Task check = await Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    cancellationToken.Token.ThrowIfCancellationRequested();
+                    SubServers.ForEach(async server =>
+                    {
+                        server.IsAvaible = server.RequestCount >= (server.Capacity * 0.20) ? false : true;
+                        if (!server.IsAvaible)
+                        {
+                            Console.WriteLine("called createSubServer.");
+                            await CreateSubServer(server.RequestCount / 2);
+                            server.RequestCount = server.RequestCount / 2;
+                            server.IsAvaible = true;
+                        }
+                        if (server.RequestCount == 0 || server.RequestCount < 0)
+                        {
+                            Console.WriteLine("sub server req count = 0");
+                            if (SubServers.Count > 2)
+                            {
+                                Console.WriteLine("thread cancelled.");
+                                server.CancellationToken.Cancel();
+                                SubServers.Remove(server);
+                            }
+                        }
+                    });
+                }
+            }
+            , cancellationToken.Token);
+            //Task hello = Task.Factory.StartNew(async () =>
+            //{
+            //    while (true)
+            //    {
+            //        cancellationToken.Token.ThrowIfCancellationRequested();
+            //        //SubServers.ForEach(async server =>
+            //        //{
+            //        //    Console.WriteLine("hello");
+            //        //    await Task.Delay(60000);
+            //        //});
+            //    }
+            //}, cancellationToken.Token);
+
+            cancellationToken.Token.Register(() =>
+            {
+                Console.WriteLine("all operations stopped.");
+                Console.WriteLine("subservers :" + SubServers.Count.ToString() + "\nMain server :" + MainServer.RequestCount.ToString());
+            });
+        }
+        public async Task CreateSubServer(int requestCount)
+        {
+            Console.WriteLine("SUBSERVER CREATED!");
+            SubServers.Add(new Server()
+            {
+                IsAvaible = true,
+                RequestCount = requestCount,
+                CancellationToken = new CancellationTokenSource(),
+                Capacity = 5000
+            });
+        }
+
+        private void btnStopRequest_Click(object sender, EventArgs e)
+        {
+            MainServer.CancellationToken.Cancel();
+            cancellationToken.Cancel();
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            cancellationToken.Cancel();
+            MainServer.CancellationToken.Cancel();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //public async Task FirstMethod(CancellationToken cancellationToken)
+        //{
+        //    if (cancellationToken.IsCancellationRequested)
+        //    {
+        //        Console.WriteLine("first thread canceled ");
+        //        return;
+        //    }
+        //    await Task.Run(async () =>
+        //    {
+        //        //await Task.Delay(7000);
+        //        Console.WriteLine("first method running");
+        //    });
+        //    Console.WriteLine("first method..");
+        //}
+        //public async Task Second(CancellationToken cancellationToken)
+        //{
+        //    if (cancellationToken.IsCancellationRequested)
+        //    {
+        //        Console.WriteLine("second thread canceled ");
+        //        return;
+        //    }
+        //    Console.WriteLine("second  method..");
+
+        //}
+
+
+    }
+}
